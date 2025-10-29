881dd9d2c0a87b8509692ade675291d4cd376c92
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-29 12:46:04 -0300

    push
    scripting next

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("3D AudioSource")]
+    [SerializeField] public AudioSource firePlace; //always awake 3D audio
+    [SerializeField] public AudioSource radio; //pause and unpause 3D audio 
+
+    [Header("2D AudioSource")]
+    [SerializeField] public AudioSource atmosphere; // if im in it's on, if im out it's off 2D audio
+    [SerializeField] public AudioSource something; //pause and unpause 2D audio
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
